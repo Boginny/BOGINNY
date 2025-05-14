0a0d77eb8fe9c97e1e17b68f16193b1475892c5d
Boginny <kupolusidavid@gmail.com>
2025-05-14 00:10:35 -0700

    'Boginny'

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,20 @@
 import { Text, View, StyleSheet } from 'react-native';
  import { Link } from 'expo-router'; 
-
+ import { Image } from 'expo-image';
+ import Button from '@/components/Button';
+ import ImageViewer from '@/components/ImageViewer'
+ 
+ const PlaceholderImage = require('@/assets/images/react-logo.png')
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
-      <Link href="/about" style={styles.button}>
-        Go to About screen
-      </Link>
+      <view style={styles.ImageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+     </view>
+     <View style= {styles.footerContainer}>
+      <Button label="Choose a photo" />
+      <Button label="Use this photo" />
+    </View>
     </View>
   );
 }
@@ -17,14 +24,18 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
   },
-  text: {
-    color: '#fff',
+  ImageContainer: {
+    flex: 1,
+    paddingTop: 28
   },
-  button: {
-    fontSize: 20,
-    textDecorationLine: 'underline',
-    color: '#fff',
+    image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+  footerContainer: {
+    flex: 1/3,
+    alignItems: 'center'
   },
 });
